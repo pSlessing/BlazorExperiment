14fa24825e07345d4128c3f19b4886856a40e3d2
Peter Slessing <psless23@student.aau.dk>
2024-04-11 20:33:21 +0200

    Add project files.

---
+++ BlazorExperiment/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorExperiment
+{
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; }
+        public string? Description { get; set; }
+        public Priority? Priority { get; set; }
+
+    }
+
+    public enum Priority
+    {
+        High = 3, Medium = 2, Low = 1
+    }
+}
