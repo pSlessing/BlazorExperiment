db385259849e26e3df80803010ebd65bc6064817
pSlessing <psless23@student.aau.dk>
2024-04-17 13:50:34 +0200

    BallDontLie

--- BlazorExperiment/CodeforPages/BallDontLieAPI.cs
+++ BlazorExperiment/CodeforPages/BallDontLieAPI.cs
@@ -9,31 +9,33 @@ namespace BlazorExperiment
     public class BallDontLieAPI
     {
         private const string baseURL = "https://api.balldontlie.io/v1";
-        private string APIKEY = "19cad8bd-dd85-4925-8e97-f66f8e48a1d8";
         private HttpClient client = new HttpClient();
         private string accessPointUrl = "";
-        public BallDontLieAPI() 
+        public BallDontLieAPI(string APIKEY) 
         {
             client.DefaultRequestHeaders.Add("Authorization", APIKEY);
         }
 
-        public async Task<NbaPlayer> getNbaPlayer(string input) 
+        public async Task<NbaPlayer> getNbaPlayer(int input) 
         {
-            accessPointUrl = "/players/" + input;
+            accessPointUrl = "/players/";
             client.BaseAddress = new Uri(baseURL + accessPointUrl);
+            var response = await client.GetAsync(input.ToString());
             
         }
 
-        public async Task<NbaPlayer> getNbaTeam(string input) 
+        public async Task<NbaPlayer> getNbaTeam(int input) 
         {
-            accessPointUrl = "/teams/" + input;
+            accessPointUrl = "/teams/";
             client.BaseAddress = new Uri(baseURL + accessPointUrl);
+            var response = await client.GetAsync(input.ToString());
         }
 
-        public async Task<NbaMatch> getNbaMatch(string input)
+        public async Task<NbaMatch> getNbaMatch(int input)
         {
-            accessPointUrl = "/games/" + input;
+            accessPointUrl = "/games/";
             client.BaseAddress = new Uri(baseURL + accessPointUrl);
+            var response = await client.GetAsync(input.ToString());
         }
 
     }
