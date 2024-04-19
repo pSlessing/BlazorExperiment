6d6a9b3e2b49064e034da83946c4328060d0ca4e
Peter Slessing <psless23@student.aau.dk>
2024-04-19 09:44:21 +0200

    Working NBA Player request

--- BlazorExperiment/CodeforPages/BallDontLieAPI.cs
+++ BlazorExperiment/CodeforPages/BallDontLieAPI.cs
@@ -8,34 +8,49 @@ namespace BlazorExperiment
 {
     public class BallDontLieAPI
     {
-        private const string baseURL = "https://api.balldontlie.io/v1";
+        private const string baseURL = "https://api.balldontlie.io/v1/";
         private HttpClient client = new HttpClient();
         private string accessPointUrl = "";
         public BallDontLieAPI(string APIKEY) 
         {
             client.DefaultRequestHeaders.Add("Authorization", APIKEY);
+            client.BaseAddress = new Uri(baseURL);
         }
 
         public async Task<NbaPlayer> getNbaPlayer(int input) 
         {
-            accessPointUrl = "/players/";
-            client.BaseAddress = new Uri(baseURL + accessPointUrl);
-            var response = await client.GetAsync(input.ToString());
+            accessPointUrl = "players/";
+            System.Diagnostics.Debug.WriteLine(accessPointUrl);
+            var response = await client.GetAsync(accessPointUrl + input.ToString());
+            var responseBody = await response.Content.ReadAsStringAsync();
+            System.Diagnostics.Debug.WriteLine(responseBody);
+            NbaPlayer returnPlayer = new NbaPlayer();
+
+
+
+            return returnPlayer;
             
         }
 
-        public async Task<NbaPlayer> getNbaTeam(int input) 
+        public async Task<NbaTeam> getNbaTeam(int input) 
         {
             accessPointUrl = "/teams/";
-            client.BaseAddress = new Uri(baseURL + accessPointUrl);
-            var response = await client.GetAsync(input.ToString());
+            var response = await client.GetAsync(accessPointUrl + input.ToString());
+            System.Diagnostics.Debug.WriteLine(response.Content);
+            NbaTeam returnTeam = new NbaTeam();
+
+
+            return returnTeam;
         }
 
         public async Task<NbaMatch> getNbaMatch(int input)
         {
             accessPointUrl = "/games/";
-            client.BaseAddress = new Uri(baseURL + accessPointUrl);
-            var response = await client.GetAsync(input.ToString());
+            var response = await client.GetAsync(accessPointUrl + input.ToString());
+            System.Diagnostics.Debug.WriteLine(response.Content);
+            NbaMatch match = new NbaMatch();
+
+            return match;
         }
 
     }
